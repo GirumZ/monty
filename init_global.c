aae73dbf249072da4174cdd5715221846b09700f
GirumZ <girum.zebe@gmail.com>
2023-06-22 02:51:03 -0700

    [Create]: a file containing global variable manipulating functions

---
+++ init_global.c
@@ -0,0 +1,24 @@
+#include "monty.h"
+/**
+ * init_global - initializes a global variable
+ */
+
+void init_global(void)
+{
+	glob.cline = 1;
+	glob.arg = NULL;
+	glob.fp = NULL;
+	glob.head = NULL;
+
+}
+/**
+ * termi_global - terminates a global variable
+ */
+
+void termi_global(void)
+{
+	free_dlistint(glob.head);
+	free(glob.buff);
+	fclose(glob.fp);
+}
+
