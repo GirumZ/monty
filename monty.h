25eb758d17761f6aa70d5c4e47806193ec917aa9
GirumZ <girum.zebe@gmail.com>
2023-06-22 02:44:57 -0700

    [Create]: a header file

---
+++ monty.h
@@ -0,0 +1,91 @@
+#ifndef MONTY_H
+#define MONTY_H
+
+#define _POSIX_C_SOURCE 200809L
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <ctype.h>
+
+/**
+ * struct stack_s - doubly linked list representation of a stack (or queue)
+ * @n: integer
+ * @prev: points to the previous element of the stack (or queue)
+ * @next: points to the next element of the stack (or queue)
+ *
+ * Description: doubly linked list node structure
+ * for stack, queues, LIFO, FIFO
+ */
+typedef struct stack_s
+{
+	int n;
+	struct stack_s *prev;
+	struct stack_s *next;
+} stack_t;
+
+
+/**
+ * struct instruction_s - opcode and its function
+ * @opcode: the opcode
+ * @f: function to handle the opcode
+ *
+ * Description: opcode and its function
+ * for stack, queues, LIFO, FIFO
+ */
+typedef struct instruction_s
+{
+	char *opcode;
+	void (*f)(stack_t **stack, unsigned int line_number);
+} instruction_t;
+/**
+ * struct global_s - global variable
+ * @cline: line number
+ * @arg: argument for opcode
+ * @fp: file pointer
+ * @head: head of a list
+ *
+ * Desctiption: global variable
+ */
+typedef struct global_s
+{
+	unsigned int cline;
+	char *arg;
+	char *buff;
+	FILE *fp;
+	stack_t *head;
+} global_t;
+
+extern global_t glob;
+
+/* file handlers */
+int check_file(int argc, char **argv);
+FILE *open_file(char *file_name);
+
+/* handles instructions */
+char *read_line(FILE *fp);
+char **tokenizer(char *string);
+
+/* handles global variable */
+void init_global(void);
+void termi_global(void);
+
+/* function selector */
+void (*find_opcode(char *opc))(stack_t **stack, unsigned int line_number);
+
+/* doubly linked list */
+stack_t *add_dnodeint(stack_t **head, const int n);
+stack_t *add_dnodeint_end(stack_t **head, const int n);
+void free_dlistint(stack_t *head);
+
+/* opcode functions */
+void push(stack_t **stack, unsigned int line_number);
+void pall(stack_t **stack, unsigned int line_number);
+void pop(stack_t **stack, unsigned int line_number);
+void pint(stack_t **stack, unsigned int line_number);
+void nop(stack_t **stack, unsigned int line_number);
+void add(stack_t **stack, unsigned int line_number);
+void swap(stack_t **stack, unsigned int line_number);
+
+#endif /* MONTY_H */
